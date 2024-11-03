5cce7c215dd7c2c1b9f012368f3c2c30dee0c1a8
petar <evgenievp@yahoo.com>
2024-11-03 17:13:18 +0200

    Starting with Java.

---
+++ src/elementaryJava/LargestPrime.java
@@ -0,0 +1,28 @@
+package elementaryJava;
+
+public class LargestPrime {
+    public static int getLargestPrime(int number) {
+        if (number <= 1) {
+            return -1;
+        }
+        int largestPrime = -1;
+        for (int i = 2; i <= number; i++) {
+            if (number % i == 0) {
+                boolean isPrime = true;
+                for (int j = 2; j <= i / 2; j++) {
+                    if (i % j == 0) {
+                        isPrime = false;
+                        break;
+                    }
+                }
+                if (isPrime) {
+                    largestPrime = i;
+                    while (number % i == 0) {
+                        number /= i;
+                    }
+                }
+            }
+        }
+        return largestPrime;
+    }
+}
--- src/elementaryJava/NumberToWords.java
+++ src/elementaryJava/NumberToWords.java
@@ -1,4 +1,58 @@
 package elementaryJava;
 
 public class NumberToWords {
+    public static int getDigitCount(int number) {
+        int count = 0;
+        if (number == 0) {
+            return 1;
+        }
+        else if (number<0){
+            return -1;
+        }
+        else{
+            while (number > 0) {
+                number = number / 10;
+                count++;
+            }
+            return count;
+        }
+    }
+    public static int reverse(int number){
+        int reverse=0;
+        int lastDigit=0;
+        int num = number;
+        while(number!=0){
+            lastDigit = number%10;
+            reverse = reverse*10 + lastDigit;
+            number = number/10;
+        }
+        return reverse;
+    }
+    public static void numberToWords(int number) {
+        if (number < 0) {
+            System.out.println("Invalid Value");
+        }
+        int remainder ;
+        int reversed = reverse(number);
+        int counted = getDigitCount(number);
+        while (counted > 0) {
+            remainder = reversed % 10;
+            String word = switch (remainder) {
+                case 0 -> "Zero";
+                case 1 -> "One";
+                case 2 -> "Two";
+                case 3 -> "Three";
+                case 4 -> "Four";
+                case 5 -> "Five";
+                case 6 -> "Six";
+                case 7 -> "Seven";
+                case 8 -> "Eight";
+                case 9 -> "Nine";
+                default -> "";
+            };
+            System.out.println(word);
+            counted--;
+            reversed/= 10;
+        }
+    }
 }
