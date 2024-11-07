620a1d902f409885aa3c2de4cb5ad77f2b80491d
petar <evgenievp@yahoo.com>
2024-11-07 10:10:34 +0200

    Arrays

--- src/Arrays/SortDescending.java
+++ src/Arrays/SortDescending.java
@@ -1,5 +1,7 @@
 package Arrays;
 import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
 import java.util.Random;
 
 
@@ -12,8 +14,18 @@ public class SortDescending {
         for (int i = 0; i < 10; i++) {
             arr[i] = random.nextInt(100);
         }
+        System.out.println(Arrays.toString(arr));
+        Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length / 2; i++) {
+            int temp = arr[i];
+            arr[i] = arr[arr.length - 1 - i];
+            arr[arr.length - 1 - i] = temp;
+        }
 
         System.out.println(Arrays.toString(arr));
+
+
     }
 
 }
